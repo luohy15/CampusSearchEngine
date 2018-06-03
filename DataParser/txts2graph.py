71f9af72d85190545f0e66108b1728081927781d
dzy <daizy15@mails.tsinghua.edu.cn>
2018-06-03 21:33:40 +0800

    python automatically detects number of txt

--- DataParser/txts2graph.py
+++ DataParser/txts2graph.py
@@ -1,11 +1,16 @@
-
-n_txt = 3
-
 fout = open("html.graph", "w", encoding="utf8")
-for i in range(1, n_txt+1):
-    with open("%s.txt"%i, "r", encoding="utf8") as fin:
-        lines = fin.readlines()
-        assert(len(lines) == 5)
-        print("%s: %s"  % (lines[0].strip(), lines[2].strip()), file=fout)
+i = 1
+while True:
+    try:
+        with open("%s.txt"%i, "r", encoding="utf8") as fin:
+            lines = fin.readlines()
+            if (len(lines) != 5):
+                print("Files should have 5 lines")
+                break
+            print("%s: %s"  % (lines[0].strip(), lines[2].strip()), file=fout)
+        i += 1
+    except:
+        print("Totally %d files found" % (i-1))
+        break
 fout.close()
 
