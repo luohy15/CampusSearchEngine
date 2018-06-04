45584289f899a7aba633c1b034aa4286c411b287
luohy15 <luohycs@gmail.com>
2018-06-04 12:37:45 +0800

    fix wrong fileid & base url

--- rawdata/totxt.py
+++ rawdata/totxt.py
@@ -18,17 +18,16 @@ def get_id(s):
 def build_fileid():
     for root, dirs, files in os.walk("."): 
         for fi in files:
+            if not (fi.endswith(".html") or fi.endswith(".htm")):
+                continue
             print(",", end="")
             sys.stdout.flush()
             get_id(root+"/"+fi)
 
 def parse_files():
+    i = 0
     for root, dirs, files in os.walk("."): 
-        #  i = 0
         for fi in files:
-            #  i = i + 1
-            #  if (i % 10 == 0):
-                #  print(i)
             #  print(".", end="")
             sys.stdout.flush()
             if not (fi.endswith(".html") or fi.endswith(".htm")):
@@ -41,37 +40,49 @@ def parse_files():
                         fr = fin.read()
                         soup = BeautifulSoup(fr)
                         print(str(fid), file=fout)
-                        print("http://" + fn[2:], file=fout)
+                        url = "http://" + fn[2:];
+                        print(url, file=fout)
                         out = []
-                        links = links_from_html(fr)
+                        links = links_from_html(fr, url)
+                        if (fid == 264):
+                            print("fid=%d"%fid)
+                            print(fr)
+                            print(text_from_html(fr))
                         for link in links:
                             fp = "./" + link[7:]
                             if fp in file2id:
                                 out.append(str(file2id[fp]))
+                        out = list(set(out))
                         print(" ".join(out), file=fout)
                         if soup.title:
                             print(soup.title.string, file=fout)
                         else:
                             print(file=fout)
-                        print(text_from_html(fr), file=fout)
+                        print(text_from_html(fr, fid), file=fout)
                 except UnicodeDecodeError:
                     with open(fn, "r", encoding="cp936") as fin:
                         fr = fin.read()
                         soup = BeautifulSoup(fr)
                         print(str(fid), file=fout)
-                        print("http://" + fn[2:], file=fout)
+                        url = "http://" + fn[2:];
+                        print(url, file=fout)
                         out = []
-                        links = links_from_html(fr)
+                        links = links_from_html(fr, url)
+                        if (fid == 264):
+                            print("fid=%d"%fid)
+                            print(fr)
+                            print(text_from_html(fr))
                         for link in links:
                             fp = "./" + link[7:]
                             if fp in file2id:
                                 out.append(str(file2id[fp]))
+                        out = list(set(out))
                         print(" ".join(out), file=fout)
                         if soup.title:
                             print(soup.title.string, file=fout)
                         else:
                             print(file=fout)
-                        print(text_from_html(fr), file=fout)
+                        print(text_from_html(fr, fid), file=fout)
                 except:
                     pass
 
--- rawdata/visible.py
+++ rawdata/visible.py
@@ -10,8 +10,21 @@ def tag_visible(element):
     return True
 
 
-def text_from_html(body):
+def text_from_html(body, fid):
     soup = BeautifulSoup(body, 'html.parser')
     texts = soup.findAll(text=True)
+    texts = filter(lambda t : False if t == '\n' else True, texts)
     visible_texts = filter(tag_visible, texts)
-    return u" ".join(t.strip() for t in visible_texts)
+    res = u" ".join(t.strip() for t in visible_texts)
+    res = res.replace("\n", "")
+    if (fid == 264):
+        print("soup:", soup)
+        #  print("text:", list(texts))
+        print("visible_texts:", res)
+
+    return res
+
+
+if __name__ == "__main__":
+    tr = urllib.request.urlopen("http://www.tsinghua.edu.cn/publish/cs/8212/index.html").read()
+    text_from_html(tr, 264)
